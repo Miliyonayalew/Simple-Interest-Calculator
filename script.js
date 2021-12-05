14de5731ddd8675c57e89b3ca57caa04cd146e8a
Miliyon Ayalew <35680940+Miliyonayalew@users.noreply.github.com>
2021-12-05 11:24:25 +0300

    final

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var p = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var year = document.getElementById("years").value;
+
+    //Check empty
+    if (p == "") {
+        alert("Please enter an amount");
+        document.getElementById("principal").focus();
+        return false;
+    } else { //Checking wether the number is positive number or not
+        if (p <= 0) {
+            alert("Please enter positive number");
+            document.getElementById("principal").focus;
+            return false;
+        }
+    }
     
+
+
+var getThe_Year = new Date().getFullYear();
+var theYear = parseInt(getThe_Year, 10) + parseInt(year, 10);
+var interest = Math.floor(p * year * rate / 100);
+var result = "If you deposit <mark>" + p +
+    "</mark>,<br> at an interest rate of <mark mark > " + rate +
+    "</mark>,<br> You will receive an amount of <mark>" + interest +
+        ",</mark> <br> in the year <mark>" + theYear + "</mark>";
+
+document.getElementById("result").innerHTML =result;
+
 }
-        
+function showVal(newVal) {
+    document.getElementById("showRate").innerHTML = newVal + '%';    
+}
